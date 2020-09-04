bd3a3a3e15dbbfad17fc7aa84afd5e331f0e45c1
princeblack <mahamadicongo@hotmail.com>
2020-09-04 16:46:41 +0200

    handling user profile group

---
+++ src/components/dashboard/Groups.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { NavLink } from "react-router-dom";
+
+export const Groups = (props) => {
+    console.log(props.data);
+    const image = props.data.imgCollection
+    return (
+        <div className="group">
+            <NavLink to={`/group/${props.data._id}`}>
+            <div
+              className="firstDiv"
+              style={{ backgroundImage: `url(${image})` }}
+            ></div>
+            </NavLink>
+            <NavLink className="link" to={`/group/${props.data._id}`}>
+                <div className="title">
+                     {props.data.groupName}
+                </div>
+            </NavLink>
+
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+
+
+export default connect(mapStateToProps)(Groups)
--- src/components/dashboard/UserPage.js
+++ src/components/dashboard/UserPage.js
@@ -4,13 +4,14 @@ import { OneUser } from "../../actions";
 import "../../scss/user.scss";
 import { NavbarToggler } from "reactstrap";
 import less from "../../img/less.svg";
-import more from "../../img/more.svg"
+import more from "../../img/more.svg";
 import moment from "moment";
+import { Groups } from "./Groups";
 
 export const UserPage = (props) => {
   const [state, setstate] = useState();
   const [info, setInfo] = useState(true);
-
+  const [group, setGroup] = useState(true);
 
   useEffect(() => {
     const id = props.match.params.id;
@@ -22,12 +23,25 @@ export const UserPage = (props) => {
       setstate(props.getOneUser.imgCollection[0]);
     }
   }, [props.getOneUser]);
-  const handleToggleInfo =(e)=>{
-      e.preventDefault();
-      setInfo(! info)
-  }
-  const date = new Date(props.getOneUser.createdAt)
+  const handleToggleInfo = (e) => {
+    e.preventDefault();
+    setInfo(!info);
+  };
+  const handleToggleGroup = (e) => {
+    e.preventDefault();
+    setGroup(!group);
+  };
+  const date = new Date(props.getOneUser.createdAt);
   const create = moment(date).format("dddd, MMMM D Y");
+  let userGroup;
+  if (props.getOneUser._id) {
+    userGroup = props.getOneUser.group.map((el,index)=>{
+        return(
+            <Groups data={el} key={el._id}></Groups>
+        )
+    })
+  }
+
   return (
     <div className="user-container">
       {props.getOneUser._id && (
@@ -50,18 +64,36 @@ export const UserPage = (props) => {
           </di>
           <div className="information">
             <div className="info-container">
-              <div className="toggle">
-                  <img src={ info ? less : more} alt="info" onClick={handleToggleInfo}></img>
-                  <h2> information</h2>
+              <div className="toggle" onClick={handleToggleInfo}>
+                <img src={info ? less : more} alt="info"></img>
+                <h2> Information</h2>
               </div>
-              {info &&(
-                  <div className="info">
-                     <p><span>Name:</span> {props.getOneUser.firstName}</p> 
-                     <p><span>Last Name: </span>{props.getOneUser.lastName}</p> 
-                     <p><span>since: </span>{create}</p> 
-                  </div>
-              )
-              }
+              {info && (
+                <div className="info">
+                  <p>
+                    <span>Name:</span> {props.getOneUser.firstName}
+                  </p>
+                  <p>
+                    <span>Last Name: </span>
+                    {props.getOneUser.lastName}
+                  </p>
+                  <p>
+                    <span>since: </span>
+                    {create}
+                  </p>
+                </div>
+              )}
+            </div>
+            <div className="user-group">
+                <div className="toggle" onClick={handleToggleGroup}>
+                  <img src={group ? less : more} alt="info"></img>
+                  <h2> Group</h2>
+                </div>
+                {
+                    group &&(
+                        userGroup
+                    )
+                }
             </div>
             <div className="publication"></div>
           </div>
